7397ba817e6e111efc904cc8c5c863f035857ba6
MarianoPtascinsky <mariano.ptascinsky@gmail.com>
2020-03-15 19:41:27 +0200

    model

---
+++ app/src/main/java/com/tasklistapp/Task.java
@@ -0,0 +1,17 @@
+package com.tasklistapp;
+
+public class Task {
+    private String nameTask;
+
+    public Task(String nameTask) {
+        this.nameTask = nameTask;
+    }
+
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
+}
